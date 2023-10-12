7368a53014cab0595641ed3862b25e8b5f18747f
Daywid <88506180+daywid@users.noreply.github.com>
2023-10-12 10:27:09 -0300

    Delete todolist/todolist directory

--- todolist/todolist/src/main/java/br/com/daywid/todolist/task/ITaskRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.daywid.todolist.task;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
-    
-}
